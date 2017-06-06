28fd63405d508f6f60d89f510bc0a9a635de676a
Yasser Hussain <yasserhussain1110@gmail.com>
2017-06-06 22:05:58 +0530

    Modified font for header. Added dependency in package.json.

--- client/App.jsx
+++ client/App.jsx
@@ -3,18 +3,15 @@ import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 import Home from './components/Home';
 import About from './components/About';
+import Header from './components/Header'
+import 'typeface-kelly-slab';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <main>
-          <h1>hello world!</h1>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
-
+          <Header/>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
         </main>
---
+++ client/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div className="header">
+    <div className="logo">
+      <div className="top-portion">CHILDREN</div>
+      <div className="bottom-portion">OF AFRICA</div>
+    </div>
+  </div>
+)
