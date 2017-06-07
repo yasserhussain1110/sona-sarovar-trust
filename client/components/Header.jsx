0cbc42691d996f94a771ab17140db548583725d2
Yasser Hussain <yasserhussain1110@gmail.com>
2017-06-07 18:36:10 +0530

    Header portion somewhat done.

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -6,5 +6,15 @@ export default () => (
       <div className="top-portion">CHILDREN</div>
       <div className="bottom-portion">OF AFRICA</div>
     </div>
+
+    <div className="nav">
+      <ul className="nav-bar">
+        <li className="list-item"><a href="/">HOME</a></li>
+        <li className="list-item"><a href="/about">ABOUT US</a></li>
+        <li className="list-item"><a href="/projects">PROJECTS</a></li>
+        <li className="list-item"><a href="/involve">GET INVOLVED</a></li>
+        <li className="list-item"><a href="/donate">DONATE</a></li>
+      </ul>
+    </div>
   </div>
-)
+);
