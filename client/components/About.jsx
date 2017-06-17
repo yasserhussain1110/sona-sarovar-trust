803b02ed55d1038c721d24f2fb6535bd2c47b30f
Yasser Hussain <yasserhussain1110@gmail.com>
2017-06-17 18:24:59 +0530

    Most work done in About page.

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
 
 const About = () => (
-  <div>
-    <h2>About Page</h2>
+  <div className="about">
+    <h2>Meet Our Team</h2>
+    <div className="person-info">
+      <div className="img-container">
+        <img className="person-img" src="/static/person.png" />
+      </div>
+
+      <div className="info">
+        <h3>Name of Person</h3>
+        <p className="info-para">
+          Some info about the person.
+          Some info about the person.
+          Some info about the person.
+        </p>
+      </div>
+    </div>
+
+    <div className="person-info">
+      <div className="img-container">
+        <img className="person-img" src="/static/person.png" />
+      </div>
+
+      <div className="info">
+        <h3>Name of Person</h3>
+        <p className="info-para">
+          Some info about the person.
+          Some info about the person.
+          Some info about the person.
+        </p>
+      </div>
+    </div>
+
+    <div className="person-info">
+      <div className="img-container">
+        <img className="person-img" src="/static/person.png" />
+      </div>
+
+      <div className="info">
+        <h3>Name of Person</h3>
+        <p className="info-para">
+          Some info about the person.
+          Some info about the person.
+          Some info about the person.
+        </p>
+      </div>
+    </div>
   </div>
 );
 
