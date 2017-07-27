3923ba34e2f5fcb812965e9da528e0b3606a4a4f
Yasser Hussain <yasserhussain1110@gmail.com>
2017-07-27 16:15:51 +0530

    Fixed prev bug

--- client/reducers/home.js
+++ client/reducers/home.js
@@ -8,14 +8,17 @@ const home = (state = defaultState, action) => {
   switch (action.type) {
     case 'RECEIVED_CENTER_PICS':
       return {
+        ...state,
         centerPics: action.centerPics
       };
     case 'RECEIVED_CENTER_PIC_CAPTIONS':
       return {
+        ...state,
         captions: action.captions
       };
     case 'RECEIVED_MAIN_TEXTS':
       return {
+        ...state,
         mainTexts: action.mainTexts
       };
     default:
