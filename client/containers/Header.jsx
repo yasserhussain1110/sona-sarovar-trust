b402a3a0e8d2714b3507aa007173313c823cedf2
Yasser Hussain <yasserhussain1110@gmail.com>
2017-09-23 20:49:33 +0530

    Changes:-
      1. Added animation to buttons
      2. Added dropdown to header links

--- client/containers/Header.jsx
+++ client/containers/Header.jsx
@@ -36,7 +36,22 @@ const HeaderView = ({match}) => (
     <div className="nav">
       <ul className="nav-bar">
         <li className="list-item">
-          <NavLink exact to={`${match.url}`} activeClassName="active">HOME</NavLink>
+          <div className="nav-link">
+            <NavLink exact to={`${match.url}`} activeClassName="active">HOME</NavLink>
+            <div className="sub-link-dropdown">
+              <ul className="sub-link-nav-bar">
+                <li className="sub-item">
+                  <NavLink to={`${match.url}/about`} activeClassName="active">History</NavLink>
+                </li>
+                <li className="sub-item">
+                  <NavLink to={`${match.url}/about`} activeClassName="active">Vision And Mission</NavLink>
+                </li>
+                <li className="sub-item">
+                  <NavLink to={`${match.url}/about`} activeClassName="active">Team</NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
         </li>
         <li className="list-item">
           <NavLink to={`${match.url}/about`} activeClassName="active">ABOUT US</NavLink>
@@ -57,30 +72,6 @@ const HeaderView = ({match}) => (
     </div>
   </div>
 );
-
-// const Header = () => (
-//   <div className="header">
-//   </div>
-// );
-
-// const Header = ({match, brandLogoUrl}) => (
-//   <div className="header">
-//     <div className="logo">
-//       <img src={brandLogoUrl} />
-//     </div>
-//
-//     <div className="nav">
-//       <ul className="nav-bar">
-//         <li className="list-item"><NavLink exact to={`${match.url}`} activeClassName="active">Home</NavLink></li>
-//         <li className="list-item"><NavLink to={`${match.url}/about`} activeClassName="active">About Us</NavLink></li>
-//         <li className="list-item"><NavLink to={`${match.url}/projects`} activeClassName="active">Projects</NavLink></li>
-//         <li className="list-item"><NavLink to={`${match.url}/activities`} activeClassName="active">Activities</NavLink></li>
-//         <li className="list-item"><NavLink to={`${match.url}/involve`} activeClassName="active">Get Involved</NavLink></li>
-//         <li className="list-item"><NavLink to={`${match.url}/donate`} activeClassName="active">Donate</NavLink></li>
-//       </ul>
-//     </div>
-//   </div>
-// );
 
 const mapStateToProps = state => ({
   brandLogoUrl: state.home.brandLogoUrl
