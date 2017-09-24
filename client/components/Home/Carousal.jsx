bc42359218ee3f1fa81e17f1d060bc545172f3b7
Yasser Hussain <yasserhussain1110@gmail.com>
2017-09-24 18:28:51 +0530

    Done window resize work

--- client/components/Home/Carousal.jsx
+++ client/components/Home/Carousal.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ImageCarousal from '../../lib/components/ImageCarousal';
 import MessageCarousal from '../../lib/components/MessageCarousal';
+import getScrollbarWidth from 'scrollbar-width';
 
-const Carousal = ({messages, imageLinks}) => (
-  <div className="carousal">
-    <ImageCarousal imageLinks={imageLinks} viewDuration={6000} />
-    <MessageCarousal messages={messages} viewDuration={3000} />
-  </div>
-);
+class Carousal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: window.innerWidth,
+      height: Math.floor(0.66 * window.innerWidth)
+    };
+
+    document.addEventListener('DOMContentLoaded', () => {
+      let width = window.innerWidth - getScrollbarWidth(true);
+      this.setState({
+        width,
+        height: Math.floor((2 * width ) / 3)
+      });
+    }, false);
+
+    window.onresize = () => {
+      let width = window.innerWidth - getScrollbarWidth(true);
+      this.setState({
+        width,
+        height: Math.floor((2 * width ) / 3)
+      });
+    };
+  }
+
+  render() {
+    const {imageLinks, messages} = this.props;
+    return (
+      <div style={this.state} className="carousal">
+        <ImageCarousal imageLinks={imageLinks} viewDuration={4000}/>
+        <MessageCarousal messages={messages} viewDuration={4000}/>
+      </div>
+    );
+  }
+}
 
 export default Carousal;
