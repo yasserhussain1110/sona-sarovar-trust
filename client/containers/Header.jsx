15d46ce52c2acbedb345e155be6682bbd3ccb5bf
Yasser Hussain <yasserhussain1110@gmail.com>
2017-09-24 16:42:37 +0530

    Changes:-
      1. Done more work on header.
         Added scroll header.
      2. Removed unwanted files.

--- client/containers/Header.jsx
+++ client/containers/Header.jsx
@@ -7,57 +7,91 @@ import SvgIcon from 'react-icons-kit';
 class Header extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      scrollState: "top"  // "top", "scrolled"
+    };
+
+
+    window.onscroll = () => {
+      if (window.scrollY > 100) {
+        this.setState({scrollState: "scrolled"});
+      } else {
+        this.setState({scrollState: "top"});
+      }
+    }
   }
 
   render() {
     return (
       <HeaderView
         match={this.props.match}
+        brandLogoUrl={this.props.brandLogoUrl}
+        scrollState={this.state.scrollState}
       />
     );
   }
 }
 
-const HeaderView = ({match}) => (
-  <div className="header">
+const HeaderView = ({match, scrollState, brandLogoUrl}) => (
+  <div className={`header ${scrollState}`}>
+    <div className="logo-holder">
+      <img src={brandLogoUrl}/>
+    </div>
     <div className="quick-links-holder">
       <div className="social-button-holder">
-        <SvgIcon className="social-buttons" size={20} icon={facebook}/>
-        <SvgIcon className="social-buttons" size={20} icon={twitter}/>
-        <SvgIcon className="social-buttons" size={20} icon={linkedin}/>
-        <SvgIcon className="social-buttons" size={20} icon={youtube}/>
+        <a href="https://www.facebook.com">
+          <SvgIcon className="social-button fb" size={20} icon={facebook}/>
+        </a>
+        <a href="https://www.twitter.com">
+          <SvgIcon className="social-button tw" size={20} icon={twitter}/>
+        </a>
+        <a href="https://www.linkedin.com">
+          <SvgIcon className="social-button li" size={20} icon={linkedin}/>
+        </a>
+        <a href="https://www.youtube.com">
+          <SvgIcon className="social-button yt" size={20} icon={youtube}/>
+        </a>
       </div>
       <div className="action-button-holder">
-        <button className="button">DONATE NOW</button>
-        <button className="button">VOLUNTEER</button>
+        <NavLink to="/web/donate" className="button donate">DONATE NOW</NavLink>
+        <NavLink to="/web/volunteer" className="button volunteer">VOLUNTEER</NavLink>
       </div>
     </div>
 
     <div className="nav">
       <ul className="nav-bar">
         <li className="list-item">
-          <div className="nav-link">
-            <NavLink exact to={`${match.url}`} activeClassName="active">HOME</NavLink>
-            <div className="sub-link-dropdown">
-              <ul className="sub-link-nav-bar">
-                <li className="sub-item">
-                  <NavLink to={`${match.url}/about`} activeClassName="active">History</NavLink>
-                </li>
-                <li className="sub-item">
-                  <NavLink to={`${match.url}/about`} activeClassName="active">Vision And Mission</NavLink>
-                </li>
-                <li className="sub-item">
-                  <NavLink to={`${match.url}/about`} activeClassName="active">Team</NavLink>
-                </li>
-              </ul>
-            </div>
-          </div>
+          <NavLink exact to={`${match.url}`} activeClassName="active">HOME</NavLink>
         </li>
         <li className="list-item">
           <NavLink to={`${match.url}/about`} activeClassName="active">ABOUT US</NavLink>
+          <div className="sub-link-dropdown">
+            <ul className="sub-link-nav-bar">
+              <li className="sub-item">
+                <NavLink to={`${match.url}/about`} activeClassName="active">HISTORY</NavLink>
+              </li>
+              <li className="sub-item">
+                <NavLink to={`${match.url}/about`} activeClassName="active">VISION AND MISSION</NavLink>
+              </li>
+              <li className="sub-item">
+                <NavLink to={`${match.url}/about`} activeClassName="active">TEAM</NavLink>
+              </li>
+            </ul>
+          </div>
         </li>
         <li className="list-item">
           <NavLink to={`${match.url}/projects`} activeClassName="active">WHAT WE DO</NavLink>
+          <div className="sub-link-dropdown">
+            <ul className="sub-link-nav-bar">
+              <li className="sub-item">
+                <NavLink to={`${match.url}/about`} activeClassName="active">HEALTH PROGRAM</NavLink>
+              </li>
+              <li className="sub-item">
+                <NavLink to={`${match.url}/about`} activeClassName="active">EDUCATION PROGRAM</NavLink>
+              </li>
+            </ul>
+          </div>
         </li>
         <li className="list-item">
           <NavLink to={`${match.url}/involve`} activeClassName="active">GET INVOLVED</NavLink>
