aef1742bff91594c30e68627275a73940e5bd93f
Yasser Hussain <yasserhussain1110@gmail.com>
2017-09-25 18:30:42 +0530

    More UI changes
    Added Media, Events etc section

---
+++ client/components/Home/MediaEventsBlog.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const MediaEventsBlog = () => (
+  <div className="media-events-blog">
+    <h2>Media, Events and Blogs</h2>
+    <div className="link-holder">
+      <div className="donate">
+        <h3>Donate to help</h3>
+        <div className="img-holder">
+          <img src="/home/IMG2.jpg"/>
+        </div>
+        <NavLink to="" className="button">Donate Now</NavLink>
+      </div>
+      <div className="events">
+        <h3>Our Events</h3>
+        <div className="img-holder">
+          <img src="/home/IMG2.jpg"/>
+        </div>
+        <NavLink to="" className="button">See More</NavLink>
+      </div>
+      <div className="blog">
+        <h3>Our Blogs</h3>
+        <div className="img-holder">
+          <img src="/home/IMG2.jpg"/>
+        </div>
+        <NavLink to="" className="button">Read Blog</NavLink>
+      </div>
+    </div>
+  </div>
+);
+
+export default MediaEventsBlog;
--- client/containers/Home.jsx
+++ client/containers/Home.jsx
@@ -1,52 +1,14 @@
 import React from 'react';
 import Carousal from '../components/Home/Carousal';
+import MediaEventsBlog from '../components/Home/MediaEventsBlog';
 import {connect} from 'react-redux';
 
 const Home = ({mainTextPara1, mainTextPara2, messages, imageLinks}) => (
   <div className="home">
     <Carousal messages={messages} imageLinks={imageLinks}/>
-
-    <div className="info">
-      <div className="short-bio">
-        <h3>Sona Sarovar Trust</h3>
-        <div className="details">
-          <p>{mainTextPara1}</p>
-          <p>{mainTextPara2}</p>
-        </div>
-      </div>
-
-      <div className="more-links">
-        <div className="link">
-          <div className="img-container">
-            <img src="/static/donate.png"/>
-          </div>
-          <div className="link-button">DONATE</div>
-          <div className="link-details">I'm a paragraph. Click here to add your own text and edit me. I’m a great place
-            for you to tell a story and let your users know a little more about you.​
-          </div>
-        </div>
-
-        <div className="link">
-          <div className="img-container">
-            <img src="/static/involve.png"/>
-          </div>
-          <div className="link-button">GET INVOLVED</div>
-          <div className="link-details">I'm a paragraph. Click here to add your own text and edit me. I’m a great place
-            for you to tell a story and let your users know a little more about you.​
-          </div>
-        </div>
-
-        <div className="link">
-          <div className="img-container">
-            <img src="/static/mission.png"/>
-          </div>
-          <div className="link-button">OUR MISSION</div>
-          <div className="link-details">I'm a paragraph. Click here to add your own text and edit me. I’m a great place
-            for you to tell a story and let your users know a little more about you.​
-          </div>
-        </div>
-      </div>
-    </div>
+    <MediaEventsBlog />
+    {/*<VolunteerParallax />*/}
+    {/*<Testimonials />*/}
   </div>
 );
 
