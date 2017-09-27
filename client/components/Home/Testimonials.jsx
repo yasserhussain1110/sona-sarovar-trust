b6ed7dc30d2f6ed712e2a47e8b0954a3954eccca
Yasser Hussain <yasserhussain1110@gmail.com>
2017-09-27 21:16:46 +0530

    added para

--- client/components/Home/Testimonials.jsx
+++ client/components/Home/Testimonials.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import {quoteLeft} from 'react-icons-kit/fa';
-import SvgIcon from 'react-icons-kit';
 
 const Testimonials = () => (
   <div className="testimonials">
     <h2>Donor Speak</h2>
-    <p></p>
-
     <blockquote>
-      {/*<SvgIcon icon={quoteLeft} size={20}/>*/}
+      <p>
+        Changing the lives of children is a continuous process.
+        Nothing can be done in a limited period.
+        As individuals we have to continue to give support by way of financial
+        help, time as well as other resources.
+        I believe in continuity and devotion without deviation. CRY is doing just that.
+      </p>
     </blockquote>
   </div>
 );
