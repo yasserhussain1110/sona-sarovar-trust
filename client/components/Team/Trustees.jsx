6829f75b2b932c8489314aebe69e4e18b9ad2ab9
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-03 18:55:40 +0530

    Added trustees sub page

--- client/components/Team/Trustees.jsx
+++ client/components/Team/Trustees.jsx
@@ -2,6 +2,52 @@ import React from 'react';
 
 const Trustees = () => (
   <div className="trustees">
+    <h1>Trustees</h1>
+    <div className="page-content">
+      <div className="trustee-info">
+        <div className="name">
+          <h2>Ratan Batliboi</h2>
+        </div>
+        <div className="designation">
+          <span>Board Chair & Hon. Trustee, Mumbai</span>
+        </div>
+        <div className="more">
+          <div className="img-holder">
+            <img src="https://www.cry.org/images/img-013.jpg" />
+          </div>
+
+          <div className="trustee-details">
+            <p>
+              Rippan’s junior in school, Ratan has had a long association with him till in 1991,
+              Ratan joined CRY as a trustee. Ever since, he has been like a steady rock for CRY.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="trustee-info">
+        <div className="name">
+          <h2>Mahnaz Curmally</h2>
+        </div>
+        <div className="designation">
+          <span>Trustee, Mumbai</span>
+        </div>
+        <div className="more">
+          <div className="img-holder">
+            <img src="https://www.cry.org/images/img-015.jpg" />
+          </div>
+
+
+          <div className="trustee-details">
+            <p>
+              A skilled writer with acknowledged strategy and consulting skills, communication experience
+              of over 30 years that extends across specialist PR practices like opinion mobilization,
+              advocacy strategy and more.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
--- client/routes/Web.jsx
+++ client/routes/Web.jsx
@@ -4,6 +4,10 @@ import Home from '../containers/Home';
 import History from '../containers/History';
 import VisionAndMission from '../containers/VisionAndMission';
 import Team from '../containers/Team';
+import Trustees from '../components/Team/Trustees';
+import BrandAmbassador from '../components/Team/BrandAmbassador';
+import TechnicalTeam from '../components/Team/TechnicalTeam';
+import Volunteers from '../components/Team/Volunteers';
 import Header from '../components/Web/Header';
 import Footer from '../components/Web/Footer';
 import Projects from '../containers/Projects';
@@ -19,7 +23,11 @@ export default ({match}) => (
     <Route exact path={`${match.url}`} component={Home}/>
     <Route path={`${match.url}/about/history`} component={History}/>
     <Route path={`${match.url}/about/vision`} component={VisionAndMission}/>
-    <Route path={`${match.url}/about/team`} component={Team}/>
+    <Route exact path={`${match.url}/about/team`} component={Team}/>
+    <Route path={`${match.url}/about/team/trustees`} component={Trustees}/>
+    <Route path={`${match.url}/about/team/brand-ambassador`} component={BrandAmbassador}/>
+    <Route path={`${match.url}/about/team/technical-team`} component={TechnicalTeam}/>
+    <Route path={`${match.url}/about/team/volunteers`} component={Volunteers}/>
     <Route exact path={`${match.url}/projects`} component={Projects}/>
     <Route path={`${match.url}/projects/:index`} component={ProjectHolder}/>
     <Route exact path={`${match.url}/activities`} component={Activities}/>
