c1b4c8b847557037c81a06bc4b054e47f9a6bc9e
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-03 18:10:22 +0530

    provided proper links

--- client/containers/Team.jsx
+++ client/containers/Team.jsx
@@ -1,50 +1,50 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-const Team = () => (
+const Team = ({match}) => (
   <div className="team">
     <h1>Team</h1>
     <div className="page-content">
       <div className="trustees sub-team">
-        <h2><NavLink to="trustees">TRUSTEES</NavLink></h2>
+        <h2><NavLink to={`${match.url}/trustees`}>TRUSTEES</NavLink></h2>
         <div className="details">
           Each one of our Trustees holds CRY’s interest above everything else,
           sharing a firm belief in values of public trust, collective …
           <div className="button-holder">
-            <NavLink to="trustees" className="button">READ MORE</NavLink>
+            <NavLink to={`${match.url}/trustees`} className="button">READ MORE</NavLink>
           </div>
         </div>
       </div>
 
       <div className="technical-team sub-team">
-        <h2><NavLink to="technical-team">TECHNICAL TEAM</NavLink></h2>
+        <h2><NavLink to={`${match.url}/technical-team`}>TECHNICAL TEAM</NavLink></h2>
         <div className="details">
           Each one of our Trustees holds CRY’s interest above everything else,
           sharing a firm belief in values of public trust, collective …
           <div className="button-holder">
-            <NavLink to="technical-team" className="button">READ MORE</NavLink>
+            <NavLink to={`${match.url}/technical-team`} className="button">READ MORE</NavLink>
           </div>
         </div>
       </div>
 
       <div className="brand-ambassador sub-team">
-        <h2><NavLink to="brand-ambassador">BRAND AMBASSADOR</NavLink></h2>
+        <h2><NavLink to={`${match.url}/brand-ambassador`}>BRAND AMBASSADOR</NavLink></h2>
         <div className="details">
           Each one of our Trustees holds CRY’s interest above everything else,
           sharing a firm belief in values of public trust, collective …
           <div className="button-holder">
-            <NavLink to="brand-ambassador" className="button">READ MORE</NavLink>
+            <NavLink to={`${match.url}/brand-ambassador`} className="button">READ MORE</NavLink>
           </div>
         </div>
       </div>
 
       <div className="volunteers sub-team">
-        <h2><NavLink to="volunteers">VOLUNTEERS</NavLink></h2>
+        <h2><NavLink to={`${match.url}/volunteers`}>VOLUNTEERS</NavLink></h2>
         <div className="details">
           Each one of our Trustees holds CRY’s interest above everything else,
           sharing a firm belief in values of public trust, collective …
           <div className="button-holder">
-            <NavLink to="volunteers" className="button">READ MORE</NavLink>
+            <NavLink to={`${match.url}/volunteers`} className="button">READ MORE</NavLink>
           </div>
         </div>
       </div>
