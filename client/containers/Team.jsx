9347f5504072a6a9bedc6b8c685442c7a78fc93f
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-03 17:49:08 +0530

    Team main page built

--- client/containers/Team.jsx
+++ client/containers/Team.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const Team = () => (
   <div className="team">
+    <h1>Team</h1>
+    <div className="page-content">
+      <div className="trustees sub-team">
+        <h2><NavLink to="trustees">TRUSTEES</NavLink></h2>
+        <div className="details">
+          Each one of our Trustees holds CRY’s interest above everything else,
+          sharing a firm belief in values of public trust, collective …
+          <div className="button-holder">
+            <button className="button">READ MORE</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="technical-team sub-team">
+        <h2><NavLink to="technical">TECHNICAL TEAM</NavLink></h2>
+        <div className="details">
+          Each one of our Trustees holds CRY’s interest above everything else,
+          sharing a firm belief in values of public trust, collective …
+          <div className="button-holder">
+            <button className="button">READ MORE</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="brand-ambassador sub-team">
+        <h2><NavLink to="ambassador">BRAND AMBASSADOR</NavLink></h2>
+        <div className="details">
+          Each one of our Trustees holds CRY’s interest above everything else,
+          sharing a firm belief in values of public trust, collective …
+          <div className="button-holder">
+            <button className="button">READ MORE</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="volunteers sub-team">
+        <h2><NavLink to="volunteers">VOLUNTEERS</NavLink></h2>
+        <div className="details">
+          Each one of our Trustees holds CRY’s interest above everything else,
+          sharing a firm belief in values of public trust, collective …
+          <div className="button-holder">
+            <button className="button">READ MORE</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
