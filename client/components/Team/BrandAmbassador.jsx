313b73e69a46eaf512f895c778a4a6dd4989db2e
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-03 17:58:00 +0530

    Added stubs for team sub views

---
+++ client/components/Team/BrandAmbassador.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const BrandAmbassador = () => (
+  <div className="brand-ambassador">
+  </div>
+);
+
+export default BrandAmbassador;
---
+++ client/components/Team/Trustees.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Trustees = () => (
+  <div className="trustees">
+  </div>
+);
+
+export default Trustees;
