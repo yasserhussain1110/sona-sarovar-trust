1d10dc20b8fb652774daab31a8c41d8a2819feb7
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-03 17:12:31 +0530

    Done work for history

--- client/containers/History.jsx
+++ client/containers/History.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
 
 const History = () => (
-  <div className="team">
+  <div className="history">
+    <h1>History</h1>
+    <div className="page-content">
+      <div className="iframe-holder">
+        <iframe src="https://www.youtube.com/embed/VT3SRGNr22I"/>
+      </div>
+
+      <div className="text-holder">
+        <p>
+          Way back in the 1970s, Rippan Kapur, a young airline purser,
+          was driven by the extraordinary dream to see a day when no
+          Indian child would be deprived of rights as basic as
+          survival, participation, protection and development.
+          Like many of us, Rippan too was upset when he saw
+          disparities between privileged and underprivileged children.
+          He hated to see children begging and working as servants.
+          Unlike most of us though, he did something about it.
+          He joined his school’s social service club and read to the blind,
+          visited children in hospitals, held reading and writing classes for
+          street children, and started a free dispensary at a slum the club adopted.
+          To raise funds for these activities, the club sold milk. It even won a
+          shield for the best Interact club! These qualities of resourcefulness and
+          determination came in handy.
+        </p>
+        <p>
+          In 1979 Rippan and 6 friends started CRY with Rs 50 around his mother’s dining table.
+          They felt that something needed to be done to improve the situation of the underprivileged Indian child.
+          Uncharacteristically, given their backgrounds and motivations, they chose not to start a grassroots-level
+          implementing organisation working directly with and for underprivileged children. They opted instead to
+          make CRY a link between the millions of Indians who could provide resources and thousands of
+          dedicated people and organisations at the grassroots-level who are struggling to function for lack of them.
+          This “link” or enabling position has determined CRY’s strategic choices at every juncture – from the
+          fundraising methods it employs, to the nature of its relationship with the NGOs it partners with.
+        </p>
+        <p>
+          All through the early, difficult years, it was Rippan’s passion and conviction that drove CRY.
+          He was firmly convinced that each of us can, in our own small way, be the agents of change, and
+          when enough of us become one, the impact is a lasting change for the better.
+          All he asked of people was that they help CRY by doing what they were good at. As he put it,
+          “What I can do, I must do.”
+        </p>
+      </div>
+
+      <div className="quote">
+        “Numbers don’t count. Even if we can change one life, it means a great deal to us.”
+        <small>Rippan Kapur, CRY Founder (1954-1994)</small>
+      </div>
+    </div>
   </div>
 );
 
