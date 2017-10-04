1dd0bbb53d9b91003f3e3b28c9a657f2f65fcabd
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-04 19:47:10 +0530

    fix for one-var

--- client/lib/helpers/functions.js
+++ client/lib/helpers/functions.js
@@ -58,8 +58,8 @@ export const clip = (str, length) => (str.length < length ? str : str.substring(
 export const getParameterByName = (name, url) => {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, '\\$&');
-  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-    results = regex.exec(url);
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+  const results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
