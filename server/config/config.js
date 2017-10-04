f4bdb23c1f40d3dcf461af5f15dbbd2e3d78b88d
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-04 19:50:00 +0530

    ignore multi assign

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,11 @@
 /* eslint-disable global-require */
+/* eslint-disable no-multi-assign */
 
 const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   require('dotenv').config();
-} else if(env === 'test') {
+} else if (env === 'test') {
   require('dotenv').config({path: '.env.test'});
 } else if (env === 'production') {
   require('dotenv').config({path: '.env.prod'});
