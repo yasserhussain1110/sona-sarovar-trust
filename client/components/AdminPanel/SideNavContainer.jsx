2268ec3daae23ac142bac728a19e7d3fb4572dec
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-18 18:03:30 +0530

    fixed side nav icons

--- client/components/AdminPanel/SideNavContainer.jsx
+++ client/components/AdminPanel/SideNavContainer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {home} from 'react-icons-kit/icomoon';
-import {calendar, user, eye, signOut} from 'react-icons-kit/fa';
+import {calendar, eye, signOut, group, star, code} from 'react-icons-kit/fa';
 import {ic_format_list_bulleted as icFormatListBulleted} from 'react-icons-kit/md/ic_format_list_bulleted';
+import {userTie} from 'react-icons-kit/icomoon/userTie';
 import SideNavBar from './SideNavContainer/SideNavBar';
 import SideNav from './SideNavContainer/SideNav';
 import {logOut} from '../../lib/handlers/commonErrorsHandler';
@@ -12,10 +13,10 @@ const SideNavContainer = () => (
     <SideNav to="/admin/about" icon={home}>About Us</SideNav>
     <SideNav to="/admin/projects" icon={icFormatListBulleted}>Projects</SideNav>
     <SideNav to="/admin/activities" icon={calendar}>Activities</SideNav>
-    <SideNav to="/admin/trustees" icon={user}>Trustees</SideNav>
-    <SideNav to="/admin/volunteers" icon={user}>Volunteers</SideNav>
-    <SideNav to="/admin/technical" icon={user}>TechnicalTeam</SideNav>
-    <SideNav to="/admin/ambassador" icon={user}>Ambassador</SideNav>
+    <SideNav to="/admin/trustees" icon={userTie}>Trustees</SideNav>
+    <SideNav to="/admin/volunteers" icon={group}>Volunteers</SideNav>
+    <SideNav to="/admin/technical" icon={code}>TechnicalTeam</SideNav>
+    <SideNav to="/admin/ambassador" icon={star}>Ambassador</SideNav>
     <SideNav to="/admin/preview" icon={eye}>Preview</SideNav>
     <SideNav to="/admin/login" onClick={logOut} icon={signOut}>Log Out</SideNav>
   </SideNavBar>
