dec4b0ce38bd2a182a95b8e14e25133fe1049e99
Yasser Hussain <yasserhussain1110@gmail.com>
2017-10-25 06:20:11 +0530

    Added Sona Sarovar Trust map

--- client/containers/Contact.jsx
+++ client/containers/Contact.jsx
@@ -28,6 +28,17 @@ const Contact = () => (
         <h2 className="add-info-header">E-mail</h2>
         <span>contact@sonasarovartrust.com</span>
       </div>
+
+      <div id="map">
+        <iframe
+          title="Sona Sarovar Trust Location"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.515544721893!2d72.81708101446887!3d19.1288974870576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b61d959b3d21%3A0x9526acafb4dbf6e8!2sRaheja+Complex+Ln%2C+Seven+Bunglow%2C+Andheri+West%2C+Mumbai%2C+Maharashtra+400047!5e0!3m2!1sen!2sin!4v1485939210544"
+          width="1200"
+          height="450"
+          frameBorder="0"
+          allowFullScreen
+        />
+      </div>
     </div>
   </div>
 );
